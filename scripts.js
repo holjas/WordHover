96e53f5caf885a23e0adec94d2afe38c3be03fe5
holjas <holly.jasiura@gmail.com>
2021-05-08 09:36:50 -0400

    make responsive

--- scripts.js
+++ scripts.js
@@ -1,21 +1,58 @@
-// import { party } from "./node_modules/party-js";
+const partyApp = {};
 
-const creativityHover = document.getElementById("creativityHover");
-const moreHover = document.getElementById("moreHover");
+partyApp.screenSize = () => {
+  const screenSize = window.innerWidth;
+  if (screenSize > 900) {
+    partyApp.largeScreen();
+    console.log("LARGE");
+  } else {
+    partyApp.smallScreen();
+    console.log("smallscreen");
+  }
+};
 
-party.resolvableShapes["heart"] = `<img src="heart.svg" class="heart"/>`;
+partyApp.largeScreen = () => {
+  const creativityHover = document.getElementById("creativityHover");
+  const moreHover = document.getElementById("moreHover");
 
-creativityHover.addEventListener("mouseenter", function (e) {
-  //   e.preventDefault();
-  party.confetti(e, {
-    shapes: ["square", "circle"],
-    count: party.variation.range(90, 100),
+  party.resolvableShapes["heart"] = `<img src="heart.svg" class="heart"/>`;
+
+  creativityHover.addEventListener("mouseenter", function (e) {
+    party.confetti(e, {
+      shapes: ["square", "circle"],
+      count: party.variation.range(90, 100),
+    });
   });
-});
-moreHover.addEventListener("mouseover", function (e) {
-  e.preventDefault();
-  party.confetti(e, {
-    shapes: ["heart"],
-    count: party.variation.range(35, 40),
+  moreHover.addEventListener("mouseover", function (e) {
+    party.confetti(e, {
+      shapes: ["heart"],
+      count: party.variation.range(35, 40),
+    });
   });
-});
+};
+
+partyApp.smallScreen = () => {
+  const creativityHover = document.getElementById("creativityHover");
+  const moreHover = document.getElementById("moreHover");
+
+  party.resolvableShapes["heart"] = `<img src="heart.svg" class="heart"/>`;
+
+  creativityHover.addEventListener("click", function (e) {
+    party.confetti(e, {
+      shapes: ["square", "circle"],
+      count: party.variation.range(90, 100),
+    });
+  });
+  moreHover.addEventListener("click", function (e) {
+    party.confetti(e, {
+      shapes: ["heart"],
+      count: party.variation.range(35, 40),
+    });
+  });
+};
+
+partyApp.init = () => {
+  partyApp.screenSize();
+};
+
+partyApp.init();
